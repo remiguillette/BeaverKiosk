96cbf066e2cdfd8697238981f0b2c0a1565d1803
Rémi Guillette <remiguillette@gmail.com>
2025-10-25 08:23:12 -0400

    Create index.ts

---
+++ src/main/index.ts
@@ -0,0 +1,17 @@
+import { QMainWindow, QWidget, QIcon } from "@nodegui/nodegui";
+import path from "path";
+import { Renderer } from "@nodegui/react-nodegui";
+import React from "react";
+import App from "../renderer/App";
+
+const win = new QMainWindow();
+win.setWindowTitle("NodeGUI Démo");
+win.resize(400, 300);
+
+const centralWidget = new QWidget();
+win.setCentralWidget(centralWidget);
+
+Renderer.render(<App />, centralWidget);
+
+win.show();
+(global as any).win = win; // Empêche le GC de fermer la fenêtre
